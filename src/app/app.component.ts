b38bed71ab4abbbebcf44968d65d182226b4c0e8
SerhiiB <44550584+sbocharnikov@users.noreply.github.com>
2020-07-09 17:15:45 +0300

    Initial implementation of register component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [],
 })
-export class AppComponent {
-  title = 'mediumclone';
-}
+export class AppComponent {}
---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'mc-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: '',
+      password: '',
+    });
+  }
+
+  onSubmit(): void {}
+}
