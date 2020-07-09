796dbdba641a13cc67b69f231ed7a5614447f09a
SerhiiB <44550584+sbocharnikov@users.noreply.github.com>
2020-07-09 17:41:48 +0300

    add ngrx store and registerAction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import {BrowserModule} from '@angular/platform-browser'
-import {NgModule} from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module'
-import {AppComponent} from './app.component'
-import {AuthModule} from './auth/auth.module'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    StoreModule.forRoot({}),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { registerAction } from '../../store/actions/register.action';
 
 @Component({
   selector: 'mc-register',
@@ -9,7 +11,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm();
@@ -23,5 +25,7 @@ export class RegisterComponent implements OnInit {
     });
   }
 
-  onSubmit(): void {}
+  onSubmit(): void {
+    this.store.dispatch(registerAction(this.form.value));
+  }
 }
