f11fe7276a0ee784c7a12d530a029b9782e6349f
SerhiiB <44550584+sbocharnikov@users.noreply.github.com>
2020-08-13 11:33:19 +0300

    implement store for feed

---
+++ src/app/shared/modules/feed/store/reducers.ts
@@ -0,0 +1,43 @@
+import { FeedStateInterface } from '../types/feedState.interface';
+import {
+  getFeedAction,
+  getFeedSuccessAction,
+  getFeedFailureAction,
+} from './actions/getFeed.action';
+import { createReducer, on, Action } from '@ngrx/store';
+
+const initialState: FeedStateInterface = {
+  isLoading: false,
+  error: null,
+  data: null,
+};
+
+const feedReducer = createReducer(
+  initialState,
+  on(
+    getFeedAction,
+    (state): FeedStateInterface => ({
+      ...state,
+      isLoading: true,
+    })
+  ),
+  on(
+    getFeedSuccessAction,
+    (state, action): FeedStateInterface => ({
+      ...state,
+      isLoading: false,
+      data: action.feed,
+    })
+  ),
+  on(
+    getFeedFailureAction,
+    (state): FeedStateInterface => ({
+      ...state,
+      isLoading: false,
+    })
+  )
+);
+
+export function reducers(state: FeedStateInterface, action: Action) {
+  return feedReducer(state, action);
+}
